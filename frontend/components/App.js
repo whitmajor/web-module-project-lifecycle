7aa39f5a443908a7594789191446268c5cd58cac
whitmajor <98010797+whitmajor@users.noreply.github.com>
2022-05-10 18:11:20 -0400

    axios call get and post successful

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,44 @@
 import React from 'react'
+import TodoList from "./TodoList"
+import axios from "axios"
 
 const URL = 'http://localhost:9000/api/todos'
 
-export default class App extends React.Component {
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      name: "",
+      id: 0,
+      completed: false,
+    
+    }
+  }
+
+  componentDidMount(){
+    axios.get(URL)
+    .then(resp=>{
+      console.log(resp.data.data)
+      const todo = resp.data.data;
+      this.setState({
+        todo: todo.name,
+      })
+      })
+  }
+postTodo = (name)=>{
+  axios.post(URL,name)
+  .then(resp=>{
+    console.log("your post",resp)
+  })
+}
+
   render() {
-    return null
+    return(
+      <TodoList/>
+
+
+
+    )
   }
 }
+export default App
